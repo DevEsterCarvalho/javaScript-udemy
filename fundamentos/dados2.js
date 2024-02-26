aac544eda64b88cef6fcca22a993dd8320c0ad0c
DevEsterCarvalho <estercarvalho205@gmail.com>
2024-02-26 19:27:37 -0300

    first commit

---
+++ fundamentos/dados2.js
@@ -0,0 +1,11 @@
+let preco = 19.90;
+let desconto = 0.4;
+
+let precoComDesconto = preco * (1- desconto);
+console.log(precoComDesconto);
+
+
+let nomeProduto = "Caderno";
+let categoria = "Papelaria";
+console.log ("Produto: " + nomeProduto + " Categoria: " + categoria + 
+    " Preço: " + preco + " Desconto: " + desconto);
---
+++ fundamentos/tipoDeDado.js
@@ -0,0 +1,10 @@
+let idade = 31;
+console.log(typeof idade);
+
+let salario = 4578.32;
+console.log(typeof salario)
+
+let chovendo = true;
+console.log(typeof chovendo);
+
+console.log(typeof "Teste");
