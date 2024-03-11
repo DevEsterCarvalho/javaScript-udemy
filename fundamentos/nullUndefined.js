87d8e90bb9e39074c9b2aba7b31bd9ab078504d5
DevEsterCarvalho <estercarvalho205@gmail.com>
2024-03-11 19:33:59 -0300

    subindo ex de null e undefined

---
+++ fundamentos/nullUndefined.js
@@ -0,0 +1,25 @@
+const a = {
+    nome: 'Teste'
+}
+
+const b = a 
+
+console.log(b)
+
+let valor 
+console.log(valor)
+
+valor = null
+console.log(valor)
+
+const produto = {}
+console.log(produto.preco)
+console.log(produto)
+
+produto.preco = undefined
+console.log(!!produto.preco)
+console.log(produto)
+
+
+produto.preco = null 
+console.log(produto)
---
+++ fundamentos/objeto.js
@@ -0,0 +1,12 @@
+const produto1 = {}
+produto1.nome = 'Celular ultra mega'
+produto1.preco = 4998.90
+
+console.log(produto1)
+
+const produto2 = {
+    nome: 'Camisa polo',
+    preco: 79.90,
+}
+
+console.log(produto2)
