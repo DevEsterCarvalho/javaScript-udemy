2a54b8f97ba419b2d34965513e6d711e80daa93f
DevEsterCarvalho <estercarvalho205@gmail.com>
2024-03-11 16:40:51 -0300

    adicionando exemplo de tipo boolean

---
+++ fundamentos/booleanos.js
@@ -0,0 +1,20 @@
+let isAtivo = false;
+console.log(isAtivo);
+
+isAtivo = true;
+console.log(isAtivo);
+
+isAtivo = 1;
+console.log(!!isAtivo);
+
+console.log(!!0);
+console.log(!!'');
+console.log(!!null);
+console.log(!!NaN);
+console.log(!!undefined);
+console.log(!!(isAtivo = false))
+
+console.log(!!('' || null || 0 || ' '))
+
+let nome = 'Lucas';
+console.log(nome || 'Desconhecido');
