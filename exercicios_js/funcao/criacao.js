cd798f3bfdbaa995e1b9c8fcd6699b48f3433c1d
DevEsterCarvalho <estercarvalho205@gmail.com>
2024-03-18 18:02:01 -0300

    adicionando formas de criar funcao

---
+++ exercicios_js/funcao/criacao.js
@@ -0,0 +1,16 @@
+function fun1 () { } //forma literal
+
+const fun2 = function () { } //dentro de uma variável
+
+const array = [function(a, b) {return a + b}, fun1, fun2] //armazenando em um array
+console.log(array[0] (2, 3))
+
+const obj = {} //armazenando em um atributo de objeto
+obj.falar = function() {return 'opa'}
+console.log(obj.falar())
+
+function run(fun) { //oassando função como parametro
+    fun()
+}
+run(function () { console.log('Executando...')})
+
