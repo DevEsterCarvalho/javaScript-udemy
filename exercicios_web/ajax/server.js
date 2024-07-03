a2defa21ad31a432ddbba0121b9af487943eafa6
DevEsterCarvalho <estercarvalho205@gmail.com>
2024-07-03 15:53:17 -0300

    editando server js

--- exercicios_web/ajax/server.js
+++ exercicios_web/ajax/server.js
@@ -5,3 +5,6 @@ const app = express()
 app.use(express.static('.'))
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
+
+app.get('/teste', (req, res) => res.send('Ok'))
+app.listen(8081, () => console.log('Executando...'))
