db5195d36022270e285d409d3d6d9f9f84f2a62d
DevEsterCarvalho <estercarvalho205@gmail.com>
2024-09-10 20:12:28 -0300

    testando gulp

--- exercicios_web/gulper/basico/gulpfile.js
+++ exercicios_web/gulper/basico/gulpfile.js
@@ -1,9 +1,30 @@
 const gulp = require ('gulp')
 const series = gulp.series
 
-function copiar (cb) {
-    console.log('Tarefa copiar')
+const antes1 = cb => {
+    console.log('Tarefa antes 1')
     return cb()
 }
 
-module.exports.default = series(copiar)
+const antes2 = cb => {
+    console.log('Tarefa antes 2')
+    return cb()
+}
+
+function copiar (cb) {
+    gulp.src(['pastaA/arquivo1.txt', 'pastaA/arquivo2.txt'])
+    .pipe(gulp.dest('pastaB'))
+    return cb()
+}
+
+const fim = cb => {
+    console.log('Tarefa fim')
+    return cb()
+}
+
+module.exports.default = series(
+    antes1, 
+    antes2,
+    copiar,
+    fim,
+)
