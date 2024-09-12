5e61bdcefeb2c546fee6e6970a807587f4e9b467
DevEsterCarvalho <estercarvalho205@gmail.com>
2024-09-12 18:28:57 -0300

    subindo projeto spa criando estrutura do build

---
+++ exercicios_web/gulper/spa/gulpTasks/servidor.js
@@ -0,0 +1,13 @@
+const gulp = require ('gulp')
+
+function monitoriarArquivos(cb) {
+    return cb()
+}
+
+function servidor(cb) {
+    return cb()
+}
+
+module.exports = {
+    monitoriarArquivos, servidor
+}
